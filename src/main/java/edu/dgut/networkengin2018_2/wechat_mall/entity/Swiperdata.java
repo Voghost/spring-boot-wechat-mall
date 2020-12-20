b8b38f7851551ac21835dabee764791f2fd20aad
ligthblue <1242021176@qq.com>
2020-12-20 10:21:49 +0800

    swiperdata实体表

---
+++ src/main/java/edu/dgut/networkengin2018_2/wechat_mall/entity/Swiperdata.java
@@ -0,0 +1,13 @@
+package edu.dgut.networkengin2018_2.wechat_mall.entity;
+
+import lombok.Data;
+
+@Data
+public class Swiperdata {
+
+    private Integer swiperId;
+    private String imageSrc;
+    private String openType;
+    private Integer goodsId;
+    private String navigatorUrl;
+}

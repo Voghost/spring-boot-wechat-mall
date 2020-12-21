104c195fdeeaf3176663e4888e1560af6d4d02fd
voghost <voghost2@gmail.com>
2020-12-21 15:06:47 +0800

    格式化时间

--- src/main/java/edu/dgut/networkengin2018_2/wechat_mall/entity/Orders.java
+++ src/main/java/edu/dgut/networkengin2018_2/wechat_mall/entity/Orders.java
@@ -1,5 +1,6 @@
 package edu.dgut.networkengin2018_2.wechat_mall.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,6 +9,7 @@ import java.util.Date;
 public class Orders {
 
     private Integer orderId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date orderTime;
     private Double orderPrice;
     private String orderAddress;
